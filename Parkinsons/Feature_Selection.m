b0eca21c2c32244e2c4fcaca579ed9b7ef4bba5a
zealshen <zealshen@outlook.com>
2019-08-20 17:37:08 +0100

    changes

--- Parkinsons/Feature_Selection.m
+++ Parkinsons/Feature_Selection.m
@@ -1,5 +1,16 @@
 clear;
 clc;
 
-%% load the data
+%% 
 load('/home/zealshen/DATA/UCI_DATA/Parkinsons/Parkinsons.mat');
+
+X = [parkinsons(:,2:17) parkinsons(:,19:24)];
+y = parkinsons(:,18);
+
+X = table2array(X);
+y = table2array(y);
+
+y = 2 * y - 1;
+
+%% IFS
+[r, w] = infFS(X, y, 0.5, 1, 0);
