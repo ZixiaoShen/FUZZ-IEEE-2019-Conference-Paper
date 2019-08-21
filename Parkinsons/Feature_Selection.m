a45c3878e7e14e2b5b58d03eff30ed8ab09a4800
zealshen <zealshen@outlook.com>
2019-08-21 14:51:18 +0100

    revise some code

--- Parkinsons/Feature_Selection.m
+++ Parkinsons/Feature_Selection.m
@@ -1,25 +1,8 @@
-<<<<<<< HEAD
 clear;
 clc;
 
 %% load the data
-load('D:/CloudFiles/OneDriveFiles/DATA/UCI_DATA/Parkinsons/parkinsons.mat');
-
-X = [parkinsons(:, 2:17) parkinsons(:, 19:24)];
-y = parkinsons(:, 18);
-
-X = table2array(X);
-y = table2array(y);
-
-y = 2 * y - 1;
-
-%% feature selection
-=======
-clear;
-clc;
-
-%% load the data
-load('/home/zealshen/DATA/UCI_DATA/Parkinsons/Parkinsons.mat');
+load('/home/zealshen/Dropbox/DATA/UCI/Parkinsons/parkinsons.mat');
 
 X = [parkinsons(:,2:17) parkinsons(:,19:24)];
 y = parkinsons(:,18);
@@ -42,14 +25,9 @@ train_y = train_data(:, Target);
 test_X = test_data(:, Vectors);
 test_y = test_data(:, Target);
 
-
 %% IFS
 [r_IFS, w_IFS] = infFS(train_X, train_y, 0.5, 1, 0);
 
 %% ILFS
 [r_ILFS, w_ILFS] = ILFS(train_X, train_y, 6, 0);
 
-
-
-
->>>>>>> a87ab7d1e9bd8dd37bc363a32ced5fba5acea21c
--- WBC/Classification.m
+++ WBC/Classification.m
@@ -2,8 +2,7 @@ clear;
 clc;
 
 %% load the data
-load('/home/zealshen/DATA/UCI_DATA/WBC/wbc.mat');
-
+load('/home/zealshen/Dropbox/DATA/UCI/WBC/wbc.mat');
 data = table2array(breastcancerwisconsin);
 
 %% data preprocessing
@@ -12,4 +11,38 @@ NaN_Row = find(isnan(data(:, NaN_Column)));
 data(NaN_Row, :) = [];
 data = data(:, 2:11);
 clear NaN_Column NaN_Row
+
+%% set up X and y
+Vectors = 1:9;
+Target = 10;
+
+X = data(:, Vectors);
+y = data(:, Target);
+y = y -3;
+data = [X y];
+%% divide the training and testing sets
+proportion = 0.67;
+[train, test] = split2train_test(data, proportion);
+
+train_X = train(:, Vectors);
+train_y = train(:, Target);
+
+test_X = test(:, Vectors);
+test_y = test(:, Target);
+
+%% Classification Process
+Mdl = fitctree(train_X, train_y);
+train_pred = predict(Mdl, train_X);
+test_pred = predict(Mdl, test_X);
+
+num_train = size(train_X, 1);
+num_test = size(test_X, 1);
+
+acc_train = sum(train_pred == train_y)/num_train;
+acc_test = sum(test_pred == test_y)/num_test;
+
+%% Feature Selection Process
+
+% IFS
+[r_IFS, w_IFS] = infFS(train_X, train_y, 0.5, 1, 0);
 
