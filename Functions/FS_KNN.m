6718b9937850e45d5789dc61730bf5109f32b1da
zealshen <zealshen@outlook.com>
2019-08-21 16:02:31 +0100

    add classification process

---
+++ Functions/FS_KNN.m
@@ -0,0 +1,33 @@
+function acc = FS_KNN(train, test, rank)
+
+    num_F = size(train, 2) - 1;
+    Vectors = 1:num_F;
+    Target = num_F + 1;
+    num_test = size(test, 1);
+    
+    train_X = train(:, Vectors);
+    train_y = train(:, Target);
+    test_X = test(:, Vectors);
+    test_y = test(:, Target);
+    
+    Mdl = fitcknn(train_X, train_y);
+    test_pred = predict(Mdl, test_X);
+    
+    acc_test = sum(test_pred == test_y)/num_test;
+    
+    acc = [acc_test];
+    rank = fliplr(rank);
+    
+    for removeN = 1:num_F-1
+        vector = Vectors;
+        vector(rank(1:removeN)) = [];
+        
+        train_M = train_X(:, vector);
+        test_M = test_X(:, vector);
+        
+        Mdl = fitcknn(train_M, train_y);
+        test_pred = predict(Mdl, test_M);
+        
+        acc_test_N = sum(test_pred == test_y)/num_test;
+        acc = [acc; acc_test_N];
+end
